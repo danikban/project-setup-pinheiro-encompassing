3856b070ef1e3688e66eec4b6e01542eed7d68b7
Lawrence <lsl359@nyu.edu>
2021-03-16 17:34:12 -0400

    login screen in app, formatting TODO

--- front-end/CopyPasta/App.js
+++ front-end/CopyPasta/App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log({code})
+  const [code, setText] = useState('');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.containerLogin}>
+      <Text styles={styles.containerHeader}>
+        CopyPasta
+      </Text>
+      <Text styles={styles.containerSubHeader}>
+        sharing. made easy.
+      </Text>
+      <TextInput
+        placeholder="Put your CopyPasta code here!"
+        onChangeText={code => setText(code)}
+        defaultValue={code}
+      />
+      <Button 
+        title= "Login"
+        onPress= {handlePress}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerLogin: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
+    alignItems: "center",
+    justifyContent:"center",
+  },
+  containerHeader: {
+    fontSize:48,
+  },
+  containerSubHeader: {
+    fontSize:24
+  },
+  containerAppText: {
+    fontSize:16,
   },
 });

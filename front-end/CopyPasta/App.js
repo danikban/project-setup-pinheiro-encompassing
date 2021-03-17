a2326689dd8cbcba78f8e9a8569f7a0822382f32
dwdankworth <dwdankworth@gmail.com>
2021-03-16 21:56:30 -0600

    connectedDevices.js

--- front-end/CopyPasta/App.js
+++ front-end/CopyPasta/App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { ConnectedDevices } from "./app";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello</Text>
     </View>
   );
 }
@@ -14,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ front-end/CopyPasta/app/ConnectedDevices.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+
+function ConnectedDevices(props) {
+  return <Text>Testing</Text>;
+}
+
+const styles = StyleSheet.create({});
+
+export default ConnectedDevices;

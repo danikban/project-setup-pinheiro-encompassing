73cab1f20b7e4ea5e02dc7f32436202740367393
Varun Bhardwaj <varunbhardwaj@Varuns-MBP.fios-router.home>
2021-03-20 17:40:04 -0400

    Fixed everything except mockaroo

--- front-end/CopyPasta/ClipboardContainer.js
+++ front-end/CopyPasta/ClipboardContainer.js
@@ -1,30 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{ useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View ,MyAppHeaderText} from 'react-native';
-import externalStyle from './style/externalStyle'
 
 const ClipboardContainer = () => {
     const [code, setText] = useState('');
     return (
         <View>
             <Text
-            style= {externalStyle.textSTYLE}
+              style={externalStyle.textSTYLE}
             >Clipboard</Text>
-
+            
             <Text
             style= {externalStyle.textCLIP1}
             >[Date/Time]</Text>
 
-        <Text
+            <Text
             style= {externalStyle.textCLIP2}
             >[Date/Time]</Text>
 
-        <Text
+            <Text
             style= {externalStyle.textCLIP3}
             >[Date/Time]</Text>
-       </View>
+        </View>
     );
-  }
+  };
+
+  const externalStyle=StyleSheet.create({
+    
+    textSTYLE:{
+        flex:.25,
+        height:80,
+        width:300,
+        color: 'blue',
+        fontSize:40,
+        backgroundColor: 'white',
+        alignItems: "center",
+        textAlign:"left",
+        textAlignVertical: "center",
+        
+        
+    },
+    textCLIP1:{
+        flex:1,
+        height:200,
+        width:300,
+        color: 'black',
+        fontSize:30,
+        
+        backgroundColor: 'white',
+        alignItems: "center",
+        textAlign:"center",
+        justifyContent: "center",
+        marginTop : 100,
+        marginBottom : 100,
+    },
+    textCLIP2:{
+        flex:1,
+        height:200,
+        width:300,
+        color: "black",
+        fontSize:30,
+        backgroundColor: "white",
+        alignItems: "center",
+        textAlign:"center",
+        marginBottom : 100,
+    },
+    textCLIP3:{
+        flex:1,
+        height:200,
+        width:300,
+        color: "black",
+        fontSize:30,
+        backgroundColor: "white",
+        alignItems: "center",
+        textAlign:"center",
+
+
+    }
+
+})
+
 
 
   export default ClipboardContainer;
+
+  

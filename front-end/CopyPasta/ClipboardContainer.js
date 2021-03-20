a8acf666d6bf013bc04f7547fa2701f0685049f6
Varun Bhardwaj <varunbhardwaj@Varuns-MBP.fios-router.home>
2021-03-19 23:48:54 -0400

    second update of ClipboardContainer

--- front-end/CopyPasta/ClipboardContainer.js
+++ front-end/CopyPasta/ClipboardContainer.js
@@ -1,62 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{ useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View ,MyAppHeaderText} from 'react-native';
-
+import externalStyle from './style/externalStyle'
 
 const ClipboardContainer = () => {
     const [code, setText] = useState('');
     return (
-        
-        <View style={styles.containerLogin}>
-            <Text styles={styles.containerHeader}>
-            Clipboard
-            </Text>
+        <View>
+            <Text
+            style= {externalStyle.textSTYLE}
+            >Clipboard</Text>
 
-            
-            
-            <TextInput 
-            placeholder="[Date/Time]"
-            onChangeText={code => setText(code)}
-            defaultValue={code}
-            />
+            <Text
+            style= {externalStyle.textCLIP1}
+            >[Date/Time]</Text>
 
-            <TextInput
-            placeholder="[Date/Time]"
-            onChangeText={code => setText(code)}
-            defaultValue={code}
-            />
+        <Text
+            style= {externalStyle.textCLIP2}
+            >[Date/Time]</Text>
 
-            <TextInput
-            placeholder="[Date/Time]"
-            onChangeText={code => setText(code)}
-            defaultValue={code}
-            />
+        <Text
+            style= {externalStyle.textCLIP3}
+            >[Date/Time]</Text>
        </View>
     );
   }
-
-
-  const styles= StyleSheet.create({
-    containerLogin: {
-      flex: 1,
-      fontSize:20,
-      backgroundColor: '#fff',
-      alignItems: "center",
-      justifyContent:"center",
-    },
-    containerHeader: {
-      fontSize:300,
-      flex: 1,
-      alignItems: "center",
-      justifyContent:"center",
-    },
-    containerSubHeader: {
-      fontSize:24
-    },
-    containerAppText: {
-      fontSize:16,
-    },
-  });
 
 
   export default ClipboardContainer;

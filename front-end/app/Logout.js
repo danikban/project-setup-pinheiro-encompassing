78af3e45f1a5c863dfc2ab47b3c1b9835b6d7ed6
danikban <db3341@nyu.edu>
2021-03-21 16:27:58 -0700

    trying to navigate to login screen

--- front-end/app/Logout.js
+++ front-end/app/Logout.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 const LogoutButton = ({ onPress }) => {
     return (
-        <TouchableOpacity style = {{ width: 100, height: 40, borderRadius: 10, backgroundColor: '#01003b', 
-                                    borderColor: 'white', justifyContent: 'end', overflow: 'hidden' }} onPress = {onPress}>
-            <Text style = {{ color: 'white', fontSize: 15, alignSelf: 'center' }}> Logout </Text>
+        <TouchableOpacity style = {{
+                width: 100,
+                height: 40,
+                borderRadius: 10,
+                backgroundColor: '#01003b',
+                borderColor: 'white',
+                justifyContent: 'end',
+                overflow: 'hidden' }}
+            onPress = {navitation.navigate('LoginScreen')}>
+            <Text style = {{ color: 'white', fontSize: 15, alignSelf: 'center' }}> 
+                Logout
+            </Text>
         </TouchableOpacity>
     );
 };

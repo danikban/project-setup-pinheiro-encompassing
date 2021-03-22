b43486fa18ba917fe90b9d2273b2342283b0f22a
danikban <db3341@nyu.edu>
2021-03-21 17:21:16 -0700

    refactored into having a style sheet

--- front-end/app/Logout.js
+++ front-end/app/Logout.js
@@ -3,22 +3,32 @@ import { TouchableOpacity, Text } from 'react-native';
 
 const LogoutButton = ({ onPress }) => {
     return (
-        <TouchableOpacity style = {{
-                width: 100,
-                height: 40,
-                borderRadius: 10,
-                padding: 10,
-                backgroundColor: '#01003b',
-                borderColor: 'white',
-                justifyContent: 'end',
-                overflow: 'hidden' }}
+        <TouchableOpacity style = {styles.button}
             // Add navigation link here Karan
             onPress = {onPress} >
-            <Text style = {{ color: 'white', fontSize: 15, alignSelf: 'center' }}> 
+            <Text style = {styles.text}> 
                 Logout
             </Text>
         </TouchableOpacity>
     );
 };
+
+const styles = StyleSheet.create({
+    button: {
+        width: 100,
+        height: 40,
+        borderRadius: 10,
+        padding: 10,
+        backgroundColor: 'white',
+        justifyContent: 'Center',
+        overflow: 'hidden',
+    },
+    text: {
+        color: 'white',
+        fontSize: 15,
+        alignSelf: 'center' ,
+    },
+    
+});
 
 export default LogoutButton;

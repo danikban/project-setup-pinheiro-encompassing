14fa71b3b56b853392280d804b42ce129c9fbb33
Lawrence <lsl359@nyu.edu>
2021-04-04 19:43:32 -0400

    numerical adjustments

--- front-end/randomnumbertesting/test.js
+++ front-end/randomnumbertesting/test.js
@@ -1,6 +1,6 @@
 function randomString() {
     var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    var charsLengthMinusOne = 35
+    var charsLengthMinusOne = chars.length - 1
     var result = ''
     for (var i = 8; i > 0; --i)
         result += chars[Math.round(Math.random() * (charsLengthMinusOne))]

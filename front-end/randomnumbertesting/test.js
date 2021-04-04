ed9d30df3619efc568facbdc0453ad9272fda2fc
Lawrence <lsl359@nyu.edu>
2021-04-04 19:17:33 -0400

    functional random string generation

--- front-end/randomnumbertesting/test.js
+++ front-end/randomnumbertesting/test.js
@@ -1,7 +1,9 @@
 function randomString() {
     var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    var result = '';
-    for (var i = 8; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-    return result;
+    var charsLengthMinusOne = 35
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
 }
-
+console.log(randomString())

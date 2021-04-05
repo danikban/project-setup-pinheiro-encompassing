f0284f3d47c7d8f2d14e8d61c4b2f2821a988872
Lawrence <lsl359@nyu.edu>
2021-04-04 20:44:15 -0400

    further randomization function

--- front-end/randomnumbertesting/test.js
+++ front-end/randomnumbertesting/test.js
@@ -7,3 +7,196 @@ function randomString() {
     return result
 }
 console.log(randomString())
+function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())function randomString() {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var charsLengthMinusOne = chars.length - 1
+    var result = ''
+    for (var i = 8; i > 0; --i)
+        result += chars[Math.round(Math.random() * (charsLengthMinusOne))]
+    return result
+}
+console.log(randomString())

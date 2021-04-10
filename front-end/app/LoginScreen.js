1ce8ccba4e8c0208f695d63fc24c2bb28133a39e
danikban <db3341@nyu.edu>
2021-04-10 11:19:39 -0700

    new firebase login

--- front-end/app/LoginScreen.js
+++ front-end/app/LoginScreen.js
@@ -11,9 +11,8 @@ import {
   KeyboardAvoidingView,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import * as Google from "expo-google-app-auth";
-import { ThemeConsumer } from "react-native-elements";
 import firebase from "firebase";
+import "firebase/auth";
 
 const LoginScreen = ({ navigation }) => {
   const [code, setText] = useState("");
@@ -116,16 +115,20 @@ const LoginScreen = ({ navigation }) => {
     });
   }
 
-  async function signInWithGoogleAsync() {
-    try {
-      const result = await Google.logInAsync({
+  async function signInWithGoogle() {
+    const googleUser = new firebase.auth.GoogleAuthProvider()
+    const auth = firebase.auth();
+    auth.signInWithPopup(googleUser).then((result) => {
+      console.log(result.user);
+    }).catch((error) => {
+      console.log(error.message)
+    }) /*({
         androidClientId:
           "978388863015-hi167o41cme3livr3vdgkoa2e4t46fia.apps.googleusercontent.com",
         iosClientId:
           "978388863015-8mr8gcj6ms0ca4rnnf0jbkfiipjlj0lq.apps.googleusercontent.com",
         scopes: ["profile", "email"],
-      });
-
+/*
       if (result.type === "success") {
         onSignIn(result);
         return result.accessToken;
@@ -134,7 +137,7 @@ const LoginScreen = ({ navigation }) => {
       }
     } catch (e) {
       return { error: true };
-    }
+    }*/
   }
   return (
     <SafeAreaView style={styles.containerLogin}>
@@ -157,7 +160,7 @@ const LoginScreen = ({ navigation }) => {
         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
       <View style={{ flex: 0.1 }} />
-      <TouchableOpacity style={styles.button} onPress={signInWithGoogleAsync}>
+      <TouchableOpacity style={styles.button} onPress={signInWithGoogle}>
         <Text style={styles.buttonText}>Google Sign In</Text>
       </TouchableOpacity>
       <View style={{ flex: 0.15 }} />

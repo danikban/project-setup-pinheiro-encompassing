b9798c37602cff974551ab46a0a968a0ba6bbebf
Whitney Dankworth <dwdankworth@gmail.com>
2021-04-18 22:43:18 -0400

    added 2 sqrt unit tests

--- UNIT-TESTING/src/app.js
+++ UNIT-TESTING/src/app.js
@@ -2,11 +2,12 @@ const add = (a = 0, b = 0) => a + b;
 const sub = (a = 0, b = 0) => a - b;
 const mul = (a = 0, b = 0) => a * b;
 const div = (a = 0, b = 0) => a / b;
-const sqrt = (a = 0) => Math.sqrt(a);
+const squareRoot = (a = 0) => Math.sqrt(a);
 
 module.exports = {
   add,
   sub,
   mul,
   div,
+  squareRoot,
 };

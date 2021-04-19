7be3e32eb5af919a3dcdb0a3ac40dd9514b92030
Whitney Dankworth <dwdankworth@gmail.com>
2021-04-18 22:27:56 -0400

    defined sqrt function

--- UNIT-TESTING/src/app.js
+++ UNIT-TESTING/src/app.js
@@ -1,8 +1,12 @@
-const add = (a=0, b=0)=> a + b;
-const sub = (a=0, b=0)=> a - b;
-const mul = (a=0, b=0)=> a * b;
-const div = (a=0, b=0)=> a / b;
+const add = (a = 0, b = 0) => a + b;
+const sub = (a = 0, b = 0) => a - b;
+const mul = (a = 0, b = 0) => a * b;
+const div = (a = 0, b = 0) => a / b;
+const sqrt = (a = 0) => Math.sqrt(a);
 
 module.exports = {
-    add,sub,mul,div
+  add,
+  sub,
+  mul,
+  div,
 };

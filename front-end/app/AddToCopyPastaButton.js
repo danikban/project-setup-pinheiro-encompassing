f72cda26cc3c03d5e51c1b7835508f4afbd6e702
Whitney Dankworth <dwdankworth@gmail.com>
2021-05-03 19:22:50 -0400

    Revert "uid displayed in console for firebse"
    This reverts commit d87ef85e541dde541d7680233bc74c1edfbbf38c.

--- front-end/app/AddToCopyPastaButton.js
+++ front-end/app/AddToCopyPastaButton.js
@@ -1,31 +1,9 @@
 import React from "react";
-import db from "./firebase.config.js";
 import { TouchableOpacity, StyleSheet, Text } from "react-native";
-import {id} from "./LoginScreen.js";
 const AddToCopyPastaButton = ({ copyText }) => {
   const cl = async () => {
     //SEND DATA TO FIREBASE HERE
-    //var users = db.ref("users");
-    console.log(id);
-    //users.orderByChild("id").on("");
-    
-    // ref.orderByChild("dimensions/height").on("child_added", function(snapshot) {
-    //   console.log(snapshot.key + " was " + snapshot.val().height + " meters tall");
-    // });
-
-    // try {
-    //   things.add({
-    //     content: "the other sample data",
-    //     date: Date.now(),
-    //     key: 1,
-    //     name: "tomato",
-    //     title: "ball",
-    //   });
-    // } catch (err) {
-    //   console.log(err);
-    // }
-
-    // console.log(copyText);
+    console.log(copyText);
   };
   return (
     <TouchableOpacity style={styles.button} onPress={cl}>
